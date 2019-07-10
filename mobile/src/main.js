6f62997c8528df497b4a12e6d594dbd74b7a3db7
root <18801295284@163.com>
2019-07-10 18:20:54 +0800

    第一次提交

---
+++ mobile/src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import 'lib-flexible/flexible.js';
+
+Vue.config.productionTip = false;
+
+new Vue({
+    router,
+    render: h => h(App)
+}).$mount('#app');
