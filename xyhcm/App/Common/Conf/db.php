6315cb7d731b004a3ccab9988aa7a9e7f9ffea2a
“g15901483708@163.com <15901483708@163.com>
2019-07-12 10:27:57 +0800

    w

--- xyhcm/App/Common/Conf/db.php
+++ xyhcm/App/Common/Conf/db.php
@@ -1,10 +1,10 @@
 <?php return array (
   'DB_TYPE' => 'mysqli',
-  'DB_HOST' => '127.0.0.1',
+  'DB_HOST' => '106.12.108.135',
   'DB_PORT' => '3306',
   'DB_USER' => 'root',
-  'DB_PWD' => 'root',
-  'DB_NAME' => 'xyhcms1',
+  'DB_PWD' => 'root123..',
+  'DB_NAME' => 'xyhcms',
   'DB_PREFIX' => 'xyh_',
   'DB_CHARSET' => 'utf8',
 );?>
