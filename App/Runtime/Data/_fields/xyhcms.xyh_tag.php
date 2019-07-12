89d8765ac329c474db12121c44e44d33dd2d7ae4
“g15901483708@163.com <15901483708@163.com>
2019-07-12 10:55:51 +0800

    w

---
+++ App/Runtime/Data/_fields/xyhcms.xyh_tag.php
@@ -0,0 +1,1 @@
+a:8:{i:0;s:2:"id";i:1;s:8:"tag_name";i:2;s:3:"num";i:3;s:3:"hit";i:4;s:4:"sort";i:5;s:8:"add_time";s:3:"_pk";s:2:"id";s:5:"_type";a:6:{s:2:"id";s:16:"int(10) unsigned";s:8:"tag_name";s:11:"varchar(30)";s:3:"num";s:16:"int(10) unsigned";s:3:"hit";s:16:"int(10) unsigned";s:4:"sort";s:16:"int(10) unsigned";s:8:"add_time";s:8:"datetime";}}
